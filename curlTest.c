499befca8979041538f88625a6b88324c0eeb236
Samantha Wu <wu.samantha.c@gmail.com>
2018-07-10 18:37:20 -0700

    added ssl support

--- curlTest.c
+++ curlTest.c
@@ -9,9 +9,15 @@ int main(void)
     curl = curl_easy_init();
     if (curl)
     {
-        curl_easy_setopt(curl, CURLOPT_URL, "0.0.0.0");
-        curl_easy_setopt(curl, CURLOPT_PORT, 5000L);
+        curl_easy_setopt(curl, CURLOPT_URL, "https://127.0.0.1");
+        curl_easy_setopt(curl, CURLOPT_PORT, 1337L);
         curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
+        curl_easy_setopt(curl, CURLOPT_SSLCERTTYPE, "PEM");
+        curl_easy_setopt(curl, CURLOPT_SSLCERT, "openssl/cert.pem");
+        curl_easy_setopt(curl, CURLOPT_SSLKEY, "openssl/key.pem");
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
+        
         ret = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
     }
